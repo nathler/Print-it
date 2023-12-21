527b697d367b85fdb336ba8eca1ea26c26ad3c4b
nathler <132166547+nathler@users.noreply.github.com>
2023-12-21 11:56:55 +0100

    ajout texte pour compréhension

--- assets/script.js
+++ assets/script.js
@@ -5,25 +5,25 @@ const slides = [
 	["slide4.jpg","Autocollants <span>avec découpe laser sur mesure</span>"]
 	];
 
-let recupBannerImage = document.querySelector(".banner-img");
-let ouMettreTexte = document.querySelector ("#banner p");
-//console.log(recupTexteImage + " ligne 11"); 
+let recupBannerImage = document.querySelector(".banner-img"); //init recup image
+let ouMettreTexte = document.querySelector ("#banner p"); //init recup texte image
+
 let currentIndex = 0;
 
 const dots = document.querySelector(".dots");
-let droite = document.querySelector(".arrow_right");
-let  gauche = document.querySelector(".arrow_left");
+let  gauche = document.querySelector(".arrow_left"); //utilisé pour le click flèche gauche ligne 24
+let droite = document.querySelector(".arrow_right"); //utilisé pour le click flèche droite ligne 32
 
 // initialisation des 4 points sous l'immage correspondant au nombre d'éléments dans le tableau
 for(let i = 0; i < slides.length; i++){
-	dots.innerHTML += '<span id="dot' + i + '" class="dot" title="Image ' + (i + 1) + '"></span>';
- }
-let recupBalisePoints = document.getElementById("dot0");
-recupBalisePoints.classList.add("dot_selected"); 
+	dots.innerHTML += '<span id="dot' + i + '" class="dot" title="Image ' + (i + 1) + '"></span>';  
+     // initialisation des textes à utiliser avec un id ici de 0 à 3, la class dot est la même pour chaque élément
+	let recupBalisePoints = document.getElementById("dot0"); //initialisation du 1er texte 
+recupBalisePoints.classList.add("dot_selected"); }
 
 gauche.addEventListener("click", function() {
 	currentIndex--;
-if(currentIndex <0){
+if(currentIndex <0){ //
     currentIndex= slides.length - 1;
 	}
 	miseAjourSlide(); 
@@ -50,10 +50,10 @@ function miseAjourSlide(){
 	//création function forEach 
 	allDots.forEach((dot, index) => {
 		//enlever la classe dot_selected 
-		dot.classList.remove('dot_selected');
+		dot.classList.remove('dot_selected'); //permet d'enlever tous les remplissages des points en retirant la classe
 		//remettre la classe lorsque l'index est égal à currentIndex
 		if (index === currentIndex) {
-		  dot.classList.add('dot_selected');
+		  dot.classList.add('dot_selected'); //permet de remettre  le remplissage du point qui correspond à l'immage
 		}
 	  });
 	}	  
